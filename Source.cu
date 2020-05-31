08c565d990c0798c846b6f0eaf0745ca18733c87
kula <kula124@gmail.com>
2020-05-31 15:09:36 +0200

    working

--- Source.cu
+++ Source.cu
@@ -25,13 +25,10 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort =
 	}
 }
 
-// FILE* fp = fopen("./wtf.txt", "w+");
-
-typedef float byte_t;
-__global__ void convolution(byte_t* pixelMap, int* filter, double coef, byte_t* resultMap, int width, int height, int channels) {
+__global__ void convolution(float* pixelMap, int* filter, double coef, float* resultMap, int width, int height, int channels) {
 	// int j = blockIdx.x * blockDim.x + threadIdx.x;
 	// int i = blockIdx.y * blockDim.y + threadIdx.y;
-	 double f[] = {0, -1, 0, -1, 5, -1, 0, -1, 0};
+	double f[] = { 0, -1, 0, -1, 5, -1, 0, -1, 0 };
 	// float f[] = {0, 0, 0, 0, 1, 0, 0, 0, 0};
 	// double f[] = {-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,-0.4,};
 	int col = threadIdx.x + blockIdx.x * blockDim.x;
@@ -64,11 +61,10 @@ __global__ void convolution(byte_t* pixelMap, int* filter, double coef, byte_t* 
 			}
 			resultMap[(row * width + col) * channels + k] = accum;
 		}
-
 	}
 }
 
-double coef[2] = { 1, 1.0 };
+// double coef[2] = { 1, 1.0 };
 int filters[2][3][3] = {
 	{
 		{1,1,1},
@@ -83,27 +79,23 @@ int filters[2][3][3] = {
 };
 
 int main(char** argv, int argc) {
-	byte_t* pixels = NULL;
-	byte_t* d_pixelMap, * d_resultMap, * h_resultMap;
-	size_t size;
+	float* d_pixelMap, * d_resultMap, * h_resultMap;
+	int size;
 	auto inputImage = importPPM("lena.ppm");
 	exportPPM("test.ppm", inputImage);
 	auto outputImage = Image_new(inputImage->width, inputImage->height, inputImage->channels);
-
-	pixels = inputImage->data;
-	h_resultMap = outputImage->data;
-	size = inputImage->width * inputImage->height * inputImage->channels;
+	size = inputImage->width * inputImage->height * inputImage->channels * sizeof(float);
 	//int* flatFilter = (int*)flattenArray((void**)filters[BoxBlur], 3, 3, sizeof(int));
 	int flatFilter[] = { 0,-1,0,-1,5,-1,0,-1,0 };
-	// byte_t* d_pixelMap, *d_resultMap, *h_resultMap;
+	// float* d_pixelMap, *d_resultMap, *h_resultMap;
 	int* d_filter;
-	gpuErrchk(cudaMalloc((void**)&d_filter, sizeof(int) * 3 * 3));
-	gpuErrchk(cudaMalloc((void**)&d_pixelMap, sizeof(byte_t) * size));
-	gpuErrchk(cudaMalloc((void**)&d_resultMap, sizeof(byte_t) * size));
+	//gpuErrchk(cudaMalloc((void**)&d_filter, sizeof(int) * 3 * 3));
+	gpuErrchk(cudaMalloc((void**)&d_pixelMap, sizeof(float) * size));
+	gpuErrchk(cudaMalloc((void**)&d_resultMap, sizeof(float) * size));
 	//---cpy
 
-	gpuErrchk(cudaMemcpy(d_pixelMap, pixels, size, cudaMemcpyHostToDevice));
-	gpuErrchk(cudaMemcpy(d_filter, flatFilter, sizeof(int) * 9, cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_pixelMap, inputImage->data, size, cudaMemcpyHostToDevice));
+	// gpuErrchk(cudaMemcpy(d_filter, flatFilter, sizeof(int) * 9, cudaMemcpyHostToDevice));
 	//DO STUFF
 	/*
 	Declare and allocate host and device memory. <
@@ -113,12 +105,13 @@ int main(char** argv, int argc) {
 	Transfer results from the device to the host. <
 	*/
 	printf("%d %d\n", inputImage->width, inputImage->height);
-	dim3 numberOfBlocks(512 / 32,512 / 32);
+	dim3 numberOfBlocks(512 / 32, 512 / 32);
 	dim3 threadsPerBlock(32, 32);
-	convolution <<<numberOfBlocks, threadsPerBlock>>> (d_pixelMap, d_filter, coef[BoxBlur], d_resultMap, inputImage->width, inputImage->height, 3);
+	cudaDeviceSynchronize();
+	convolution << <numberOfBlocks, threadsPerBlock >> > (d_pixelMap, NULL, 0.0, d_resultMap, inputImage->width, inputImage->height, 3);
 
 	gpuErrchk(cudaPeekAtLastError());
-	h_resultMap = (byte_t*)malloc(sizeof(byte_t) * size);
+	h_resultMap = (float*)malloc(sizeof(float) * 512 * 512 * 3);
 
 	gpuErrchk(cudaMemcpy(h_resultMap, d_resultMap, size, cudaMemcpyDeviceToHost));
 
@@ -126,12 +119,11 @@ int main(char** argv, int argc) {
 	// loadbmp_encode_file("lena2.bmp", h_resultMap, width, height, LOADBMP_RGB);
 	outputImage->data = h_resultMap;
 	exportPPM("output.ppm", outputImage);
-	
-	free(pixels);
+
 	free(h_resultMap);
 	// free(flatFilter);
 	// fclose(fp);
-	cudaFree(d_filter);
+	// cudaFree(d_filter);
 	cudaFree(d_resultMap);
 	cudaFree(d_pixelMap);
 	return 0;
